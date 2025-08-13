f1df1159e32497b16ee760381105f6f4dbae772b
Awujo <awujodarlington0@gmail.com>
2025-08-13 12:01:42 +0200

    db

--- api/db.php
+++ api/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'mysql-19b972d0-hackerxy-4d45.g.aivencloud.com';  
 $port = '18217';    // or your DB host
-$db   = 'okoh';  // your actual database name
+$db   = 'defaultdb';  // your actual database name
 $user = 'avnadmin';  // your DB username
 $pass = 'AVNS_RfFfn4ze5auGVOAtIfF';  // your DB password
 
