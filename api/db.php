bc5b8a2e410d2cfafb23e58e986439dee4c70c94
Awujo <awujodarlington0@gmail.com>
2025-08-13 11:57:23 +0200

    db

--- api/db.php
+++ api/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';      // or your DB host
+$host = 'mysql-19b972d0-hackerxy-4d45.g.aivencloud.com';  
+$port = '18217';    // or your DB host
 $db   = 'okoh';  // your actual database name
-$user = 'root';  // your DB username
-$pass = '';  // your DB password
+$user = 'avnadmin';  // your DB username
+$pass = 'AVNS_RfFfn4ze5auGVOAtIfF';  // your DB password
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 // Check connection
 if ($conn->connect_error) {
