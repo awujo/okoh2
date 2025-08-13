f6062c4d2c39b21e1a1a2cbfc1022d72c47e351c
Awujo <awujodarlington0@gmail.com>
2025-08-13 19:19:35 +0200

    getin

--- api/auth.php
+++ api/auth.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'session.php';
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['email']) || $_SESSION['2fa_verified'] !== true) {
-    header("Location: ../getIn/login.html");
+    header("Location: ../getin/login.html");
     exit;
 } else {
     include_once 'db.php';
