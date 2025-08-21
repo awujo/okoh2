ad6d2355bf259287c8d1d936198b1c81f5e0cc0e
Awujo <awujodarlington0@gmail.com>
2025-08-21 19:06:52 +0200

    depost for okoh

--- api/db.php
+++ api/db.php
@@ -1,18 +1,18 @@
 <?php
-$host = 'mysql-19b972d0-hackerxy-4d45.g.aivencloud.com';  
-$port = '18217';    // or your DB host
-$db   = 'defaultdb';  // your actual database name
-$user = 'avnadmin';  // your DB username
-$pass = 'AVNS_RfFfn4ze5auGVOAtIfF';  // your DB password
+// $host = 'mysql-19b972d0-hackerxy-4d45.g.aivencloud.com';  
+// $port = '18217';    // or your DB host
+// $db   = 'defaultdb';  // your actual database name
+// $user = 'avnadmin';  // your DB username
+// $pass = 'AVNS_RfFfn4ze5auGVOAtIfF';  // your DB password
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+// $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// $host = 'localhost';  // or your DB host
-// $db   = 'okoh';  // your actual database name
-// $user = 'root';    // your DB username             
-// $pass = '';    // your DB password
+$host = 'localhost';  // or your DB host
+$db   = 'okoh';  // your actual database name
+$user = 'root';    // your DB username             
+$pass = '';    // your DB password
 
-// $conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
