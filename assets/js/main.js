b115fe509ebc804d6cabc5e7fd0c1954d8887a41
Awujo Darlington <71509142+awujo@users.noreply.github.com>
2026-06-11 17:37:45 +0200

    svg chang

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/logo_black.png';
+          logo.src = 'assets/images/logo/logo_black.svg';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/logo_white.png';
+          logo.src = 'assets/images/logo/logo_white.svg';
         }
 
         // show or hide the back-top-top button
